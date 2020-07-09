06c45655e3edf65a2ef868f609e4e0f3a513b42a
beolaa45 <beolaa45@users.noreply.github.com>
2020-07-09 10:13:55 +0700

    a

--- pug-cli/js/home.js
+++ pug-cli/js/home.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    console.log("ok")
-
+    
     /////// BNANER
     $('.bamner-carousell').slick({
         dots: true,
@@ -9,5 +8,22 @@ $(document).ready(function(){
     $(".slick-arrow.slick-next").text('').append(`<i class="fas fa-chevron-right"></i>`)
     $(".slick-arrow.slick-prev").text('').append(`<i class="fas fa-chevron-left"></i>`)
 
-    $('.slick-dots li button').text('')
+    $('.slick-dots li button').text('');
+
+    ////// BANNER LIST
+
+    $(window).on("load resize", function(){
+        
+       let widthPhoto = $(".js-photo-1").width();
+       let heightPhoto = widthPhoto;
+       $(".js-photo-1").height(heightPhoto)
+       
+       let widthPhoto2 = $(".js-photo-2").width();
+       let heightPhoto2 = widthPhoto2/2;
+       $(".js-photo-2").height(heightPhoto2)
+    
+       let widthPhoto3 = $(".js-photo-3").width();
+       let heightPhoto3 = widthPhoto3;
+       $(".js-photo-3").height(heightPhoto3)
+    })
   });
