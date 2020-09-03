717ecd148f96ea5969e50611cadfbcb483356a97
beolaa45 <beolaa45@users.noreply.github.com>
2020-09-03 14:47:03 +0700

    app

--- manga/js/index.js
+++ manga/js/index.js
@@ -1,17 +1,15 @@
-$( document ).ready(function() {
-    let API_ROOT = "http://localhost:3000"
-    let request = $.ajax({
-        url: `${API_ROOT}/users`,
-        method: "GET",
-        // data: { id : menuId },
-        // dataType: "html"
-      });
-       
-      request.done(function( data ) {
-        console.log("data",data);
+$(document).ready(function () {
+  let API_ROOT = "http://localhost:3000";
+  let request = $.ajax({
+    url: `${API_ROOT}/users`,
+    method: "GET"
+  });
 
-        let html = data.map(value => {
-            return `<tr>
+  request.done(function (data) {
+    console.log("data", data);
+
+    let html = data.map((value) => {
+      return `<tr>
             <td>${value.name}</td>
             <td>${value.birthday}</td>
             <td>${value.email}</td>
@@ -19,54 +17,44 @@ $( document ).ready(function() {
             <td>
                 <a class="table_link" href="./edit.html?id=${value.id}">
                     <i class="fas fa-edit"></i>Chỉnh sửa 
-
                 </a>
                 |
-                <a class="table__link-delete" data-toggle="modal" data-value=${JSON.stringify(value.id)} data-target="#exampleModalLong" >
+                <a class="table__link-delete" data-toggle="modal" data-value=${JSON.stringify(
+                  value.id
+                )} data-target="#exampleModalLong" >
                     <i class="fas fa-trash-alt"></i>
                     Xóa
                 </a>
             </td>
-        </tr>`
-        })
-       
-        $(".tbody").html(html.join(''));
+        </tr>`;
+    });
 
-        $(".table__link-delete").click(function(){
-        let value =  $(this).data("value");
-        console.log(value)
-         $(".modal-body").html(`<h1>${value}</h1>`)
-            
-        })
-        $(".btn-primary").click(function(){
-          let index =  +$(".modal-body h1").text()
-          console.log(typeof index);
-          
-          let request = $.ajax({
-            url: `${API_ROOT}/users/${index}`,
-            method: "DELETE",
-          
-          })
+    $(".tbody").html(html.join(""));
 
-          request.done(function(data){
-            console.log(data)
-            window.location.reload()
-          })
+    $(".table__link-delete").click(function () {
+      let value = $(this).data("value");
+      console.log(value);
+      $(".modal-body").html(`<h1>${value}</h1>`);
+    });
+    $(".btn-primary").click(function () {
+      let index = +$(".modal-body h1").text();
+      console.log(typeof index);
 
-        })
-        
+      let request = $.ajax({
+        url: `${API_ROOT}/users/${index}`,
+        method: "DELETE",
       });
-      
-      request.fail(function( jqXHR, textStatus ) {
-        alert( "Request failed: " + textStatus );
-      }); 
 
-   
-/////////////////////////////
-
-
-      
+      request.done(function (data) {
+        console.log(data);
+        window.location.reload();
+      });
+    });
   });
 
+  request.fail(function (jqXHR, textStatus) {
+    alert("Request failed: " + textStatus);
+  });
 
-
+  /////////////////////////////
+});
