a4030f8ea14168b515de60b5c6daf12ca09546e9
beolaa45 <beolaa45@users.noreply.github.com>
2020-09-04 04:34:11 +0700

    app

---
+++ student/public/js/add.js
@@ -0,0 +1,21 @@
+console.log(1);
+let API_ROOT = "http://localhost:3000";
+$(".btn-success").click(function () {
+  let data = {
+    name: $("#name").val(),
+    email: $("#email").val(),
+
+    phone: $("#phone").val(),
+    birthday: $("#birthYear").val(),
+  };
+
+  console.log(data);
+  $.ajax({
+    type: "Post",
+    url: `${API_ROOT}/users`,
+    dataType: "json",
+    data: data,
+  }).done(function (data) {
+    window.location.href = "index.html";
+  });
+});
---
+++ student/public/js/edit.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  let API_ROOT = "http://localhost:3000";
+
+  let id = $(location).attr("search");
+  let arr = id.split("=");
+  id = arr[arr.length - 1];
+
+  let request = $.ajax({
+    url: `${API_ROOT}/users/${id}`,
+    method: "GET",
+  });
+
+  request.done(function (data) {
+    console.log("data", data);
+
+    $("#name").val(data.name);
+    $("#birthYear").val(data.birthday);
+    $("#email").val(data.email);
+    $("#phone").val(data.phone);
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    alert("Request failed: " + textStatus);
+  });
+
+  $("#btn-save").click(function (e) {
+    let data = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+
+      phone: $("#phone").val(),
+      birthday: $("#birthYear").val(),
+    };
+
+    console.log(data);
+
+    $.ajax({
+      type: "Patch",
+      url: `${API_ROOT}/users/${id}`,
+      dataType: "json",
+      data: data,
+    }).done(function (data) {
+      window.location.href = "index.html";
+    });
+  });
+});
