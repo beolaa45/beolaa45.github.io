438e7f89dc789dec01dc112349b03c8cb5805d67
beolaa45 <beolaa45@users.noreply.github.com>
2020-09-13 20:09:12 +0700

    app

--- html-template/js/Summary.js
+++ html-template/js/Summary.js
@@ -1,12 +1,17 @@
-
-function Summary({data}){
-    return(
-        <div className="summary">
-        <ul>
-          <li>Subtotal <span>${data.subTotal}</span></li>
-    <li>Tax <span>${data.tax}</span></li>
-          <li className="total">Total <span>${data.total}</span></li>
-        </ul>
-      </div>
-    )
+function Summary({ data }) {
+  return (
+    <div className="summary">
+      <ul>
+        <li>
+          Subtotal <span>${data.subTotal}</span>
+        </li>
+        <li>
+          Tax <span>${data.tax}</span>
+        </li>
+        <li className="total">
+          Total <span>${data.total}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- html-template/js/app.js
+++ html-template/js/app.js
@@ -20,45 +20,42 @@ let PRODUCTS = [
 let SUMMARY = {
   subTotal: null,
   tax: 0.2,
-  total: null
+  total: null,
 };
 
 for (const value of PRODUCTS) {
-  SUMMARY = {...SUMMARY, subTotal: SUMMARY.subTotal += value.price * value.quantity}
+  SUMMARY = {
+    ...SUMMARY,
+    subTotal: (SUMMARY.subTotal += value.price * value.quantity),
+  };
 }
-SUMMARY = {...SUMMARY, total: SUMMARY.subTotal *(1 - SUMMARY.tax ) }
+SUMMARY = { ...SUMMARY, total: SUMMARY.subTotal * (1 - SUMMARY.tax) };
 console.log(SUMMARY);
 
 const App = (props) => {
-const onChange = (e, id) => {
-  console.log(e.target.value, id)
- let index =  PRODUCTS.findIndex(obj =>  obj.id == id);
- console.log(index);
+  const onChange = (e, id) => {
+    console.log(e.target.value, id);
+    let index = PRODUCTS.findIndex((obj) => obj.id == id);
+    console.log(index);
 
- PRODUCTS = [
-   ...PRODUCTS,
-  
- ]
+    console.log(PRODUCTS);
+  };
 
- console.log(PRODUCTS)
- 
-}
-
- return (
-  <main>
-    <CartHeader header="Shopping Cart" title="Home" />
-    <section className="container">
-      <Product products={PRODUCTS} handleChange={onChange} />
-    </section>
-    <section className="container">
-      <Promotion />
-      <Summary data={SUMMARY} />
-      <div className="checkout">
-        <Button />
-      </div>
-    </section>
-  </main>
-);
-} 
+  return (
+    <main>
+      <CartHeader header="Shopping Cart" title="Home" />
+      <section className="container">
+        <Product products={PRODUCTS} handleChange={onChange} />
+      </section>
+      <section className="container">
+        <Promotion />
+        <Summary data={SUMMARY} />
+        <div className="checkout">
+          <Button />
+        </div>
+      </section>
+    </main>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
