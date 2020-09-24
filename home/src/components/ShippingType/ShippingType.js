c5c4d64bbea5791bee512db3a3c5c938c4eded80
Khanh <khanh@opentechiz.com>
2020-09-24 17:02:31 +0700

    app

--- home/src/components/ShippingType/ShippingType.js
+++ home/src/components/ShippingType/ShippingType.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./ShippingType.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function ShippingType({}) {
+function ShippingType({title, subtitle , icon}) {
     return (
-        <div>
-            123
+        <div className="ShippingType">
+            <FontAwesomeIcon icon={icon} className="ShippingType__icon"/>
+            <div className="ShippingType__content">
+                <h3 className="ShippingType__title">
+                    {title}
+                </h3>
+                <p className="ShippingType__subTitle">
+                    {subtitle}
+                </p>
+            </div>
         </div>
     )
 }
--- home/src/pages/Home/Home.js
+++ home/src/pages/Home/Home.js
@@ -2,7 +2,9 @@ import React, { Fragment, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ShippingType from "../../components/ShippingType/ShippingType";
 import Button from "../../components/UI/Button/Button";
+import { faLifeRing, faRecycle, faUser } from '@fortawesome/free-solid-svg-icons'
 import "./Home.scss";
+import Card from "../../components/Card/Card";
 function Home(props) {
   return (
     <Fragment>
@@ -28,15 +30,59 @@ function Home(props) {
       <section className="Shipping">
           <Container>
             <Row>
-              <Col xl={4} lg={4}>
-                <div>
-                  <ShippingType />
+              <Col xl={4} lg={4} md={12} sm={12} xs={12}>
+                <div className="Shipping__content">
+                  <ShippingType icon={faLifeRing} title="SUPPORT 24/7" subtitle="we support 24 hours a day"/>
                 </div>
               </Col>
-              <Col xl={4} lg={4}>1</Col>
-              <Col xl={4} lg={4}>1</Col>
+              <Col xl={4} lg={4} md={12} sm={12} xs={12}>
+              <div className="Shipping__content">
+                  <ShippingType icon={faRecycle} title="30 DAYS RETURN" subtitle="you have 30 days to return"/>
+                </div>
+              </Col>
+              <Col xl={4} lg={4} md={12} sm={12} xs={12}>
+              <div className="Shipping__content">
+                  <ShippingType icon={faUser} title="PAYMENT 100% SECURE" subtitle="Payment 100% Secure"/>
+                </div>
+              </Col>
             </Row>
           </Container>
+      </section>
+      <section className="Category">
+            <Container>
+              <Row>
+                <Col xl={4} lg={4} md={4} sm={12} xs={12}>
+                    <div className="Category__card">
+                      <Card
+                        title="Audio" 
+                        url="https://cdn.shopify.com/s/files/1/0332/6420/5963/files/cat15_720x.jpg?v=1582857819"/>
+                    </div>
+                </Col>
+                <Col xl={4} lg={4} md={4} sm={12} xs={12}>
+                <div className="Category__card">
+                      <Card
+                        title="Speaker" 
+                        url="https://cdn.shopify.com/s/files/1/0332/6420/5963/files/cat13_720x.jpg?v=1582796459"/>
+                    </div>
+                </Col>
+                <Col xl={4} lg={4} md={4} sm={12} xs={12}>
+                <div className="Category__card">
+                      <Card
+                        title="Hi-end" 
+                        url="https://cdn.shopify.com/s/files/1/0332/6420/5963/files/hi-end_720x.jpg?v=1583147788"/>
+                    </div>
+                </Col>
+              </Row>
+            </Container>
+      </section>
+      <section className="BestSeller">
+                <Container>
+                  <Row>
+                    <Col>
+                      123
+                    </Col>
+                  </Row>
+                </Container>
       </section>
     </Fragment>
   );
