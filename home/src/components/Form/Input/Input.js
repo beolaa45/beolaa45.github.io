304ac627eab338a09e8b2fb41781f1f458105745
beolaa45 <beolaa45@users.noreply.github.com>
2020-10-17 17:07:09 +0700

    app

---
+++ home/src/components/Form/Input/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Input(props) {
+    return (
+       <input style={{padding: "0 1rem"}} {...props}/>
+    )
+}
+
+export default Input
+
--- home/src/components/UI/Button/Button.js
+++ home/src/components/UI/Button/Button.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react'
 import "./Button.scss"
-function Button({children, classN, style={}, loading, clicked}) {
+function Button({children, classN, style={}, loading, clicked, type="button"}) {
     let button = ["Button"]
     if(classN) button.push(classN)
     return (
       
-            <button  onClick={clicked} className={button.join(" ")} style={style}>
+            <button type={type}  onClick={clicked} className={button.join(" ")} style={style}>
             {children}
             </button>
         
